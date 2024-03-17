b0b36f777fd57d802f44e9cb6d8ad391b96a0b7d
РђРЅРґСЂРµР№ РЎРµРґРµР»СЊРЅРёРєРѕРІ <7netmail@mail.ru>
2024-03-18 01:58:24 +0600

    РґРѕРїРѕР»РЅРёР» Р·Р°РґР°С‡Рё

--- yatube/ice_cream/views.py
+++ yatube/ice_cream/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+# posts/views.py
 from django.http import HttpResponse
 
 
+# Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'ice_cream/index.html'
+    return render(request, template)
 
 
 def ice_cream_list(request):
